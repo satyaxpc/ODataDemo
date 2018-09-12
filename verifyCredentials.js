ce0b0294c266397d15f0c1601118e3c50c0a8fdf
satyaxpc@gmail.com <satyaxpc@gmail.com>
2018-09-12 14:45:05 +0530

    test

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,3 +1,15 @@
-'use strict';
+"use strict";
+const request = require('request-promise');
+const messages = require('elasticio-node').messages;
+module.exports = verify;
+const accountService = require("./lib/commons/services/accountService.js");
+/***
+ * @param credentials object to retrieve apiKey from
+ *
+ * @returns Promise sending HTTP request and resolving its response
+ */
+async function verify(credentials) {
+    return await accountService.login(credentials);
+}
 
-module.exports = require('./lib/commons/odata/ODataClient').verifyCredentialsFactory(require('./lib/genericODataClient'));
+
