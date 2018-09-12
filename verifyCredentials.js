6e93eb8bdb5eda278f7d20b17da322dd83d9e44f
satyaxpc@gmail.com <satyaxpc@gmail.com>
2018-09-12 15:37:32 +0530

    Test

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,15 +1,15 @@
-// "use strict";
-// const request = require('request-promise');
-// const messages = require('elasticio-node').messages;
-// module.exports = verify;
-// const accountService = require("./lib/commons/services/accountService.js");
-// /***
-//  * @param credentials object to retrieve apiKey from
-//  *
-//  * @returns Promise sending HTTP request and resolving its response
-//  */
-// async function verify(credentials) {
-//     return await accountService.login(credentials);
-// }
+"use strict";
+const request = require('request-promise');
+const messages = require('elasticio-node').messages;
+module.exports = verify;
+const accountService = require("./lib/commons/services/accountService.js");
+/***
+ * @param credentials object to retrieve apiKey from
+ *
+ * @returns Promise sending HTTP request and resolving its response
+ */
+async function verify(credentials) {
+    return await accountService.login(credentials);
+}
 
 
