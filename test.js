17c4735491f1ecb95788338c833120e2c542b809
satyaxpc@gmail.com <satyaxpc@gmail.com>
2018-09-14 04:28:05 +0530

    tes

--- test.js
+++
@@ -1,9 +0,0 @@
-const messages = require('elasticio-node').messages;
-const contactService = require('../ODataDemo/lib/commons/services/contactService.js');
-exports.process = processTrigger;
-
-async function processTrigger(cfg){
-    await contactService.retrieveContact(cfg);
-}
-
-processTrigger({endPointUrl : 'https://api.constantcontact.com', api_key : 'zkh6y8tat9b8pa7ypcza6kv9'});
